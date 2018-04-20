db811842ebc1ba1355a2c874b9097817c907c0c7
Austin Lee <heero916@hotmail.com>
2018-04-20 15:55:52 -0700

    1

--- prototype_interactions.js
+++ prototype_interactions.js
@@ -1,33 +1,32 @@
 $("#bubbleButton").click(function() {
-if($('#bubble').is(':hidden')) {
-$("#bubble").css("display", "block");
-}
-else {
-$("#bubble").css("display", "none");
-}
-}
+		if ($('#bubble').is(':hidden')) {
+			$("#bubble").css("display", "block");
+		} else {
+			$("#bubble").css("display", "none");
+		}
+	}
 
 );
 $(".conarrative-button-close").click(function() {
-if($('#bubble').is(':hidden')) {
-$("#bubble").css("display", "block");
-} else {
-$("#bubble").css("display", "none");}
-});
-;
+	if ($('#bubble').is(':hidden')) {
+		$("#bubble").css("display", "block");
+	} else {
+		$("#bubble").css("display", "none");
+	}
+});;
 // ID of the Google Spreadsheet
-var spreadsheetID="1FokJsNlBlRqEDY1Sk2YAlJPhWNd3JFyl5nceyvDpvpo";
+var spreadsheetID = "1FokJsNlBlRqEDY1Sk2YAlJPhWNd3JFyl5nceyvDpvpo";
 // Make sure the spreadsheet is published to the web
-var url="https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-var str=window.location.pathname;
-str=str.substring(str.indexOf("/") + 1);
-str=str.substring(str.indexOf("/") + 1);
-addressURL=str.substring(str.indexOf("/") + 1);
+var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+var str = window.location.pathname;
+str = str.substring(str.indexOf("/") + 1);
+str = str.substring(str.indexOf("/") + 1);
+addressURL = str.substring(str.indexOf("/") + 1);
 $.getJSON(url, function(data) {
-var entry=data.feed.entry[0];
-var text = entry["gsx$"+addressURL].$t;
-text = text.replace(/(?:\r\n|\r|\n)/g, '<br />');
-$('#addressURL').prepend(text);
-}
+		var entry = data.feed.entry[0];
+		var text = entry["gsx$" + addressURL].$t;
+		text = text.replace(/(?:\r\n|\r|\n)/g, '<br />');
+		$('#addressURL').prepend(text);
+	}
 
 );
