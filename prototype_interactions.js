0b49b323d9b625bc71c36864b01558b634ca57dd
Austin Lee <heero916@hotmail.com>
2018-04-23 19:22:51 -0700

    Added Flash Interactions

--- prototype_interactions.js
+++ prototype_interactions.js
@@ -1,3 +1,4 @@
+// Clicking the Floating button will toggle visibility
 $("#bubbleButton").click(function () {
 	if ($('#bubble').is(':hidden')) {
 		$("#bubble").css("display", "block");
@@ -5,23 +6,45 @@ $("#bubbleButton").click(function () {
 		$("#bubble").css("display", "none");
 	}
 }
-
 );
+
+// Clicking the top-right close button will toggle visibility
 $(".conarrative-button-close").click(function () {
 	if ($('#bubble').is(':hidden')) {
 		$("#bubble").css("display", "block");
 	} else {
 		$("#bubble").css("display", "none");
 	}
-});;
+});
+
+// Clicking 'Flash' will cause clickable elements to change colors
+$("#flashInteractions").click(function () {
+	$('a>*').css({
+		"filter": "brightness(0%)",
+		"transition": "all 0.2s"
+	});
+	setTimeout(function () {
+		$('a>*').css("filter", "brightness(200%)");
+	}, 300);
+	setTimeout(function () {
+		$('a>*').css("filter", "brightness(100%)");
+	}, 600);
+}
+);
+
 // ID of the Google Spreadsheet
 var spreadsheetID = "1FokJsNlBlRqEDY1Sk2YAlJPhWNd3JFyl5nceyvDpvpo";
+
 // Make sure the spreadsheet is published to the web
 var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
 var str = window.location.pathname;
+
+// Gets the last string of the url after '/'
 str = str.substring(str.indexOf("/") + 1);
 str = str.substring(str.indexOf("/") + 1);
 addressURL = str.substring(str.indexOf("/") + 1);
+
+// Parses through the JSON
 $.getJSON(url, function (data) {
 	var interactions = data.feed.entry[0];
 	try {
